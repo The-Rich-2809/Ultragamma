ba67dfad3f21435f4aa4f0e60ef40574bc680ab1
Ricardo Prado Remigio <97078181+The-Rich-2809@users.noreply.github.com>
2023-04-20 11:50:06 -0600

    Botones productos
    Solución a error de imágenes e incorporación de botones a los productos

--- Program.cs
+++ Program.cs
@@ -18,6 +18,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Productos}/{action=Productos}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
