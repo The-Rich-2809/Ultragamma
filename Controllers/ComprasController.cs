1927b11e10e73fc5a8ef158bf277137e7f6abc51
Ricardo Prado Remigio <97078181+The-Rich-2809@users.noreply.github.com>
2023-05-14 08:17:44 -0600

    Direcciones
    Vista de Direcciones

---
+++ Controllers/ComprasController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ultragamma.Models;
+
+namespace Ultragamma.Controllers
+{
+    public class ComprasController : Controller
+    {
+        public IActionResult Direcciones()
+        {
+            ViewBag.Header = RegistrosModel.Header();
+            return View();
+        }
+    }
+}
