d6447ac677d287417b34930e0ce45eee858f3181
juannava18 <120217215+juannava18@users.noreply.github.com>
2023-05-28 12:40:49 -0600

    Presentacion

---
+++ Ultragamma Presentacion/Ultragamma Presentacion/Ultragamma Presentacion/Controllers/UltragammaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ultragamma_Presentacion.Controllers
+{
+    public class UltragammaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult QuienesSomos()
+        {
+            return View();
+        }
+        public IActionResult Objetivos()
+        {
+            return View();
+        }
+        public IActionResult Alcances()
+        {
+            return View();
+        }
+        public IActionResult Titulo()
+        {
+            return View();
+        }
+        public IActionResult Carrusel()
+        {
+            return View();
+        }
+        public IActionResult Materias()
+        {
+            return View();
+        }
+    }
+}
